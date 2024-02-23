30e861e181a888ac773d9b58f0759de6e8c31440
Moises Almeida <moisesssprg@gmail.com>
2024-02-23 16:37:32 -0300

    feat: adicionando devise

---
+++ spec/system/users/user_access_account_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Usuário acessa página de login' do
+  it 'e faz login com sucesso' do
+    create(:user, email: 'zelda@nintendo', password: '123456')
+
+    visit root_path
+    click_on 'Fazer login!'
+    fill_in 'Email', with: 'zelda@nintendo'
+    fill_in 'Senha', with: '123456'
+    click_on 'Entrar'
+
+    expect(page).to have_content('Login efetuado com sucesso')
+    expect(page).to have_content('zelda@nintendo')
+    expect(page).to have_button('Sair')
+  end
+end
