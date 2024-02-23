1201b3a931957637e9b4d5d2e47862be247e1558
Moises Almeida <moisesssprg@gmail.com>
2024-02-23 17:06:50 -0300

    Adiciona teste e implementação de login

--- spec/system/users/user_access_account_spec.rb
+++ spec/system/users/user_access_account_spec.rb
@@ -5,13 +5,13 @@ describe 'Usuário acessa página de login' do
     create(:user, email: 'zelda@email.com', password: '123456')
 
     visit root_path
-    click_on 'Fazer login!'
-    fill_in 'Email', with: 'zelda@nintendo'
-    fill_in 'Senha', with: '123456'
-    click_on 'Entrar'
+    click_on 'Faça login!'
+    fill_in 'Email', with: 'zelda@email.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
 
-    expect(page).to have_content('Login efetuado com sucesso')
-    expect(page).to have_content('zelda@nintendo')
+    expect(page).to have_content('Signed in successfully')
+    expect(page).to have_content('zelda@email.com')
     expect(page).to have_button('Sair')
   end
 end
